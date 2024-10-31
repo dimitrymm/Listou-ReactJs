29bce96fec505ef807683d4efc541fc6626471f2
Dimitry <dimitrymmarinho@gmail.com>
2024-10-31 15:48:00 -0300

    Update sliderData.tsx

--- src/lib/sliderData.tsx
+++ src/lib/sliderData.tsx
@@ -2,22 +2,22 @@ const sliderData = [
   {
     id: 1,
     title: "Fresh Beef Steak",
-    url: "./src/assets/images/Designer.jpeg",
+    url: "https://images.unsplash.com/photo-1551183053-bf91a1d81141",
   },
   {
     id: 2,
-    title: "Fresh Beef Steak",
-    url: "./src/assets/images/carrot.jpg",
+    title: "Carrot",
+    url: "https://images.unsplash.com/photo-1522187698837-ffbd4cf8c661",
   },
   {
     id: 3,
-    title: "Fresh Beef Steak",
-    url: "./src/assets/images/legumes.jpeg",
+    title: "Legumes",
+    url: "https://images.unsplash.com/photo-1518976024611-3f60eec7e5d1",
   },
   {
     id: 4,
-    title: "Fresh Beef Steak",
-    url: "./src/assets/images/tomate.jpeg",
+    title: "Tomato",
+    url: "https://images.unsplash.com/photo-1561047029-3000e71a92f7",
   },
 ];
 export default sliderData;
