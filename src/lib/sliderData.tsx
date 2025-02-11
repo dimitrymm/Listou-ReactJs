0d4d6d7fa3e65e2a279e66010ea8386d7e0bf3f3
Dimitry <dimitrymmarinho@gmail.com>
2025-02-11 14:58:14 -0300

    Update sliderData.tsx

--- src/lib/sliderData.tsx
+++ src/lib/sliderData.tsx
@@ -1,28 +1,28 @@
 const sliderData = [
   {
     id: 1,
-    title: "Fresh Beef Steak",
-    url: "./public/images/01.jpg",
+    title: "Uma imagem de carne bovina crua.",
+    url: "https://tse2.mm.bing.net/th?id=OIP.W9u945z31XM-55FjFg4TXAHaE8&w=316&h=316&c=7",
   },
   {
     id: 2,
-    title: "Fresh Beef Steak",
-    url: "./public/images/02.jpg",
+    title: "Uma imagem de carne bovina crua.",
+    url: "https://tse2.mm.bing.net/th?id=OIP.5qCPGNP4fFkTW57HMcxamgHaHa&w=474&h=474&c=7",
   },
   {
     id: 3,
-    title: "Fresh Beef Steak",
-    url: "./public/images/03.jpg",
+    title: "Uma imagem de legumes frescos.",
+    url: "https://tse2.mm.bing.net/th?id=OIP.OiXfmg4W6dFV2NZ-Au5YLgHaHa&w=474&h=474&c=7",
   },
   {
     id: 4,
-    title: "Fresh Beef Steak",
-    url: "./public/images/04.jpg",
+    title: "Uma imagem de frutas variadas.",
+    url: "https://tse2.mm.bing.net/th?id=OIP.KkYpfRGIfwr3yLlur9SgygHaE7&w=200&h=133&c=7",
   },
   {
     id: 5,
-    title: "Fresh Beef Steak",
-    url: "./public/images/05.jpg",
+    title: "Uma imagem de um mercado de alimentos.",
+    url: "https://tse2.mm.bing.net/th?id=OIP.1g4LwMyt6RW3U9rBr7YIMgHaFr&w=200&h=153&c=7",
   },
 ];
 export default sliderData;
